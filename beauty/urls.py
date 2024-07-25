383d68d6e497a481a2ac526dc1000f5957410891
Aleksandr Skripko <skripkoae@gmail.com>
2024-07-25 10:50:18 +0300

    джанга версии3 и старше не поддерживает фукнцию url, заменено на path/repath

--- beauty/urls.py
+++ beauty/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import include, re_path
 from django.contrib import admin
 
 
 urlpatterns = [
     # url(r'^$', views.show_flats),
     # url(r'^search/$', views.show_flats),
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
 ]
